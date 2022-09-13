1d69708c200c5392483e7348533b59dea9294328
Цунд Сюн <30247967+bigjunda@users.noreply.github.com>
2022-09-13 20:34:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
+echo 'src-git passwall1 https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
 echo 'src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci' >> feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2'>> feeds.conf.default
